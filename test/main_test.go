e35eff3fee6d86270b8bee716705af419b818675
Anthony Volkov <anthony.volkov@strongdm.com>
2025-05-29 12:30:38 -0400

    Fix env vars

--- test/main_test.go
+++ test/main_test.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"os"
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/terraform"
@@ -10,7 +11,19 @@ import (
 func TestTerraformBasic(t *testing.T) {
 	opts := &terraform.Options{
 		TerraformDir: "../", // Path to your Terraform code
+		Vars: map[string]interface{}{
+			"aws_region": os.Getenv("TF_VAR_aws_region"),
+			"subnet_id":  os.Getenv("TF_VAR_subnet_id"),
+			"vpc_id":     os.Getenv("TF_VAR_vpc_id"),
+			"name":       os.Getenv("TF_VAR_name"),
+		},
+		EnvVars: map[string]string{
+			"TF_VAR_tags": os.Getenv("TF_VAR_tags"),
+		},
 	}
+
+	// Cleanup at the end
+	defer terraform.Destroy(t, opts)
 
 	// Init and apply
 	terraform.InitAndApply(t, opts)
@@ -23,7 +36,4 @@ func TestTerraformBasic(t *testing.T) {
 	assert.Contains(t, output, "Name")
 	assert.Contains(t, output, "ManagedBy")
 	assert.Contains(t, output, "Application")
-
-	// Cleanup
-	defer terraform.Destroy(t, opts)
 }
